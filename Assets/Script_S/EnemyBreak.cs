4bd6f3c4957bec194677ff75552338a0660272e7
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2018-09-03 14:49:02 +0900

    タイトルシーンリザルトシーン追加

---
+++ Assets/Script_S/EnemyBreak.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoxelImporter
+{
+    public class EnemyBreak : MonoBehaviour
+    {
+        float lifeTime = 1f;
+
+        bool isExplositon = false;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(isExplositon)Explosion();
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            if(col.tag == "test")
+            {
+                isExplositon = true;
+            }
+        }
+
+        void Explosion()
+        {
+            var colliders = Physics.OverlapSphere(transform.position, 1f);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                var skinnedVoxelExplosion = colliders[i].GetComponent<VoxelSkinnedAnimationObjectExplosion>();
+
+                var voxelExplosion = colliders[i].GetComponent<VoxelBaseExplosion>();
+                if (voxelExplosion == null) continue;
+
+                if (!voxelExplosion.enabled)
+                {
+                    var rigidbody = colliders[i].GetComponent<Rigidbody>();
+                    var rigidbodyEnabled = false;
+                    if (rigidbody != null)
+                    {
+                        rigidbodyEnabled = rigidbody.isKinematic;
+                        rigidbody.isKinematic = true;
+                    }
+                    var collider = colliders[i];
+                    collider.enabled = false;
+
+                    voxelExplosion.SetExplosionCenter(voxelExplosion.transform.worldToLocalMatrix.MultiplyPoint3x4(transform.position));
+
+                    voxelExplosion.ExplosionPlay(lifeTime, () => Destroy(voxelExplosion.gameObject));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script_S/Result.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour {
+
+    public Sprite[] resultSp;
+
+    public SpriteRenderer resultRenderer;
+
+    public Text scoreText;
+
+    public Text highScoreText;
+
+    string scoreKey = "HIGH_SCORE";
+
+    int score = 2;
+
+	// Use this for initialization
+	void Start () {
+        SpriteSet();
+
+        int highScore = PlayerPrefs.GetInt(scoreKey);
+
+        scoreText.text = "スコア       :  " + score.ToString();
+
+        highScoreText.text = "ハイスコア:  " + highScore.ToString();
+
+        HighScore();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void SpriteSet()
+    {
+        if (score <= 1) resultRenderer.sprite = resultSp[0];
+        else resultRenderer.sprite = resultSp[1];
+    }
+
+    void HighScore()
+    {
+        int highScore = PlayerPrefs.GetInt(scoreKey);
+
+        if(highScore < score) PlayerPrefs.SetInt(scoreKey, score);
+    }
+}
